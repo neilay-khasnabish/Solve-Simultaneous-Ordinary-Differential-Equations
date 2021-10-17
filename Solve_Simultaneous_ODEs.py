609be224e32154ae90fd7765f0e4d23815b4a243
Neilay Khasnabish <37937561+neilay-khasnabish@users.noreply.github.com>
2021-10-17 22:38:22 +0530

    Basic script
    Basic script

--- Solve_Simultaneous_ODEs.py
+++ Solve_Simultaneous_ODEs.py
@@ -3,7 +3,7 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Define interms of state space
+# Define the equation
 def vdp(t, z):
     x, y, z = z
     u0 = np.sin(t) + np.cos(t)
@@ -11,15 +11,22 @@ def vdp(t, z):
     u2 = 1 # Kept constnt
     return [-x - y*z + u0, -y + x*z + u1, -z - x*y + u2]
 
-def genData(a, b, c) : 
-  t = linspace(0, 20, 200)
-  u0 = np.sin(t) + np.cos(t)
-  u1 = np.cos(t) * 10
-  u2 = 1 # Kept constnt
+# Initial values
+a = 1
+b = 1
+c = 1
 
-  # Solve
-  sol = solve_ivp(vdp, y0 = [a, b, c], t_span = [0, 20], t_eval=t)
-  finalOP_y = sol.y[0] + sol.y[1] + sol.y[2]
-  finalOP_y = finalOP_y / 3.0
+# Create time stamps
+t = linspace(0, 20, 200)
 
-  return u0, u1, u2, finalOP_y
+# Generate input signals
+u0 = np.sin(t) + np.cos(t)
+u1 = np.cos(t) * 10
+u2 = 1 # Kept constnt
+
+# Solve
+sol = solve_ivp(vdp, y0 = [a, b, c], t_span = [0, 20], t_eval=t)
+finalOP_y = sol.y[0] + sol.y[1] + sol.y[2]
+finalOP_y = finalOP_y / 3.0
+
+# Variable finalOP_y contains the final output
